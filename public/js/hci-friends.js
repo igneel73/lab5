c72669ef3c47e93eed21d3358d78b4cf3b86345e
igneel73 <igneel.kush73@gmail.com>
2019-02-07 12:41:27 -0800

    completed lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,7 +3,12 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
+
+$('.friend a.name h3').click(function(e){
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
+});
 
 /*
  * Function that is called when the document is ready.
